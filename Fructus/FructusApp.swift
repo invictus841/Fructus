d2369144977328e9f8c691528512c13915894f38
NT <usedforfunny@gmail.com>
2022-03-30 10:53:56 +0200

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Alexandre Talatinian on 30/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
